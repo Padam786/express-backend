312dee920e145cf36ffe3d58c4dea9fd86fc322f
Padam7890 <padamthapamoho@gmail.com>
2024-06-07 17:29:50 +0545

    new

---
+++ doublefunc.js
@@ -0,0 +1,13 @@
+
+const car = (a) => {
+   return a
+}
+
+const ball = (n) => {
+    return n;
+}
+
+module.exports = {
+    Car:car,
+    Ball:ball
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+
+//require files from different sources
+const divide = require('./divide.js');
+const multiply = require('./multiply.js');
+const subtract = require('./subtract.js');
+const sum = require('./sum.js');
+
+
+// divide from divide.js
+console.log(divide(3,4))
+
+// add from sum.js  
+
+console.log(sum(3,4))
+
+
+// substract from substract.js
+
+console.log(subtract(3,4))
+
+// multiply from multiply.js
+
+console.log(multiply(3,4))
+
+
+
+
